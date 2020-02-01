a6ba301f8be84e2eb09a9967f2d83195910bbf0d
Anuj Vasil <avasil@usc.edu>
2020-02-01 15:06:30 -0800

    some ui

--- Straw/Straw/ViewController.swift
+++ Straw/Straw/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func continueButton(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text{
+            Auth.auth().signIn(withEmail: email, password: password){
+                (user, error)
+                in
+                
+                if error != nil {
+                    //create account
+                }
+                else{
+                    self.performSegue(withIdentifier: "toFeed", sender: nil)
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
